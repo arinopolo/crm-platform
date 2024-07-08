64e2a2c9896dfc9d7c09b814c91401a40cc6d9ee
Arina Sedova <arinopololo@gmail.com>
2024-07-08 22:41:01 +0200

    initial commit

---
+++ components/layout/Sidebar.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+
+<template>
+  <aside>
+    <NuxtLink to="/">
+      <NuxtImg src="logo.svg" alt="logo" width="100px" />
+    </NuxtLink>
+  </aside>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+console.log("Loaded");
+</script>
+
+<template>
+  <section class="grid height">
+    <LayoutSidebar />
+    <div>
+      <slot />
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.grid {
+  display: grid;
+  grid-template-columns: 1fr 6fr;
+}
+.height {
+  min-height: 100vh;
+}
+</style>
